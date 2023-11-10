75ac40cc263b0a08ae70f89f36655f7739959307
Barney Hill <barneyh@NDPH9082.local>
2023-11-10 16:16:56 +0000

    removed try except wrapping program

--- aminos.py
+++ aminos.py
@@ -14,57 +14,53 @@ def main():
     # Configure logging
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-    try:
-        vcf = aminos.io.VCF(args.vcf)
-        gff = aminos.io.GFF(args.gff)
-        transcript_references = aminos.io.fasta.read_transcript_references(args.fasta)
+    vcf = aminos.io.VCF(args.vcf)
+    gff = aminos.io.GFF(args.gff)
+    transcript_references = aminos.io.fasta.read_transcript_references(args.fasta)
 
-        gff_transcripts = gff.get_unique_transcripts()
+    gff_transcripts = gff.get_unique_transcripts()
 
-        for transcript_id in gff_transcripts:
-            if transcript_id not in transcript_references:
+    for transcript_id in gff_transcripts:
+        if transcript_id not in transcript_references:
+            continue
+        else:
+            transcript_reference = transcript_references[transcript_id]
+
+        mutations = aminos.processing.mutations.Mutations(transcript_reference)
+        chr, start, end = gff.get_transcript_range(transcript_id)
+
+        logging.info(f"Processing transcript: {transcript_id} ({chr}:{start}-{end})")
+
+        for record in vcf(f'{chr}:{start}-{end}'):
+            bcsq = record.INFO.get('BCSQ')
+            if bcsq is None:
                 continue
-            else:
-                transcript_reference = transcript_references[transcript_id]
 
-            mutations = aminos.processing.mutations.Mutations(transcript_reference)
-            chr, start, end = gff.get_transcript_range(transcript_id)
+            for csq in bcsq.split(','):
+                mutation = aminos.processing.csq.process_csq(transcript_id, csq)
 
-            logging.info(f"Processing transcript: {transcript_id} ({chr}:{start}-{end})")
-
-            for record in vcf(f'{chr}:{start}-{end}'):
-                bcsq = record.INFO.get('BCSQ')
-                if bcsq is None:
+                if not mutation:
                     continue
 
-                for csq in bcsq.split(','):
-                    mutation = aminos.processing.csq.process_csq(transcript_id, csq)
+                for individual_call, individual in zip(record.genotypes, vcf.samples):
+                    for haplotype in [0, 1]:
+                        if individual_call[haplotype] == 1:
+                            mutations.add_sample_mutation(f'{individual}_{haplotype}', mutation)
 
-                    if not mutation:
-                        continue
+        file = aminos.io.fasta.Writer(args.output, transcript_id)
 
-                    for individual_call, individual in zip(record.genotypes, vcf.samples):
-                        for haplotype in [0, 1]:
-                            if individual_call[haplotype] == 1:
-                                mutations.add_sample_mutation(f'{individual}_{haplotype}', mutation)
+        for individual in vcf.samples:
+            for haplotype in [0, 1]:
+                sample_name = f'{individual}_{haplotype}'
+                seq = mutations.concat_mutations(sample_name)
 
-            file = aminos.io.fasta.Writer(args.output, transcript_id)
+                file.write_header(sample_name)
+                file.write_sequence(seq)
 
-            for individual in vcf.samples:
-                for haplotype in [0, 1]:
-                    sample_name = f'{individual}_{haplotype}'
-                    seq = mutations.concat_mutations(sample_name)
-
-                    file.write_header(sample_name)
-                    file.write_sequence(seq)
-
-            file.close()
-
-    except Exception as e:
-        logging.error(f"An error occurred: {e}")
+        file.close()
 
 # example:
-# python3 aminos.py --vcf data/example.vcf.gz --gff data/Homo_sapiens.GRCh38.110.chromosome.21.gff3.gz --fasta data/reference_sequences.fasta.gz --output data/test
+# python3 aminos.py --vcf data/ALL_GGVP.chr21.vcf.gz --gff data/Homo_sapiens.GRCh38.110.chromosome.21.gff3.gz --fasta data/reference_sequences.fasta.gz --output data/test
 
 if __name__ == "__main__":
     main()
