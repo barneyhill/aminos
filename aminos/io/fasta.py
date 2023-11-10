3ea80f88be97e40ed07e84e10ed2d445a86efcc6
Barney Hill <barneyh@NDPH9082.local>
2023-11-10 14:59:38 +0000

    Added logging to gff, fasta

--- aminos/io/fasta.py
+++ aminos/io/fasta.py
@@ -1,38 +1,61 @@
 import gzip
 import os
+import logging
 
 def read_transcript_references(file_path):
+    logging.info(f"Reading transcript references from: {file_path}")
     transcript_reference = {}
-    with gzip.open(file_path, 'rt') as file:  # 'rt' mode to read as text
-        name = None
-        seq_list = []
-        for line in file:
-            line = line.strip()  # Remove trailing newline characters
-            if line.startswith(">"):
-                if name:  # If not the first sequence, save the previous sequence
-                    transcript_reference[name] = ''.join(seq_list)
-                    seq_list = []
-                name = line[1:].split()[0]  # Get the name, discard the ">"
-            else:
-                seq_list.append(line)
-        if name:  # Save the last sequence
-            transcript_reference[name] = ''.join(seq_list)
+    try:
+        with gzip.open(file_path, 'rt') as file:  # 'rt' mode to read as text
+            name = None
+            seq_list = []
+            for line in file:
+                line = line.strip()  # Remove trailing newline characters
+                if line.startswith(">"):
+                    if name:  # If not the first sequence, save the previous sequence
+                        transcript_reference[name] = ''.join(seq_list)
+                        seq_list = []
+                    name = line[1:].split()[0]  # Get the name, discard the ">"
+                else:
+                    seq_list.append(line)
+            if name:  # Save the last sequence
+                transcript_reference[name] = ''.join(seq_list)
+    except Exception as e:
+        logging.error(f"Error in reading transcript references: {e}")
+        raise
     return transcript_reference
 
 class Writer:
 
     def __init__(self, file_dir, transcript):
         self.file_path = os.path.join(file_dir, f'{transcript}.fa.gz')
-        self.file = gzip.open(self.file_path, 'w')
+        try:
+            self.file = gzip.open(self.file_path, 'w')
+            logging.info(f"Initialized Writer for file: {self.file_path}")
+        except Exception as e:
+            logging.error(f"Error opening file for writing: {e}")
+            raise
 
     def write_header(self, individuals):
-        if (type(individuals) == str) or len(individuals) == 1:
-            self.file.write(f'>{individuals}\n'.encode())
-        elif type(individuals) == list and len(individuals) > 1:
-            self.file.write(f'>{",".join(individuals)}\n'.encode())
+        try:
+            if (type(individuals) == str) or len(individuals) == 1:
+                self.file.write(f'>{individuals}\n'.encode())
+            elif type(individuals) == list and len(individuals) > 1:
+                self.file.write(f'>{",".join(individuals)}\n'.encode())
+        except Exception as e:
+            logging.error(f"Error writing header: {e}")
+            raise
     
     def write_sequence(self, sequence):
-        self.file.write(f'{sequence}\n'.encode())
+        try:
+            self.file.write(f'{sequence}\n'.encode())
+        except Exception as e:
+            logging.error(f"Error writing sequence: {e}")
+            raise
 
     def close(self):
-        self.file.close()
+        try:
+            self.file.close()
+        except Exception as e:
+            logging.error(f"Error closing file: {e}")
+            raise
--- aminos/io/gff.py
+++ aminos/io/gff.py
@@ -1,27 +1,38 @@
 import pandas as pd
+import logging
 
 class GFF:
     def __init__(self, file):
+        logging.info(f"Initializing GFF with file: {file}")
         self.gff = self.read_gff(file)
 
     @staticmethod
     def read_gff(file):
+        logging.info(f"Reading GFF file: {file}")
         field_names = [
             'seqid', 'source', 'type', 'start', 'end', 'score', 
             'strand', 'phase', 'attributes'
         ]
-        gff = pd.read_csv(file, sep='\t', comment='#', names=field_names)
-        gff['ID'] = gff['attributes'].str.extract(r'ID=transcript:([^;]+)')
-        gff = gff[gff['ID'].notna()]
-        return gff
+        try:
+            gff = pd.read_csv(file, sep='\t', comment='#', names=field_names)
+            gff['ID'] = gff['attributes'].str.extract(r'ID=transcript:([^;]+)')
+            gff = gff[gff['ID'].notna()]
+            logger.info("Successfully read and processed GFF file")
+            return gff
+        except Exception as e:
+            logger.error(f"Error in reading GFF file: {e}")
+            raise
 
     def get_transcript_range(self, transcript_id):
+        logging.info(f"Getting transcript range for ID: {transcript_id}")
         transcript = self.gff[self.gff['ID'] == transcript_id]
 
         if transcript.empty:
+            logging.warning(f"No transcript found for ID: {transcript_id}")
             return None, None, None
         
         if transcript.shape[0] > 1:
+            logging.error(f"More than one transcript found for ID: {transcript_id}")
             raise Exception('Unsupported more than 1 transcript')
 
         return (
@@ -31,4 +42,6 @@ class GFF:
         )
 
     def get_unique_transcripts(self):
-        return self.gff['ID'].unique()
+        unique_transcripts = self.gff['ID'].unique()
+        logging.info(f"Number of unique transcripts: {len(unique_transcripts)}")
+        return unique_transcripts
