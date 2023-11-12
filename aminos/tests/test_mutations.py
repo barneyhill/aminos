1bfd8378c98c530d661cee450a26c0d6be38625d
Barney Hill <barneyh@NDPH9082.local>
2023-11-12 16:44:45 +0000

    fix tests

--- aminos/tests/test_mutations.py
+++ aminos/tests/test_mutations.py
@@ -9,20 +9,28 @@ class TestConcatMutations(unittest.TestCase):
         self.mutations = Mutations(self.transcript_reference)
 
     def test_simple_mutation(self):
-        self.mutations.add_sample_mutation("sample1", Mutation("M", 3, "G", 3, "T"))
+        mut_id = self.mutations.get_mutation_id(Mutation("M", 3, "G", 3, "T"))
+        self.mutations.add_sample_mutation("sample1", mut_id)
         self.assertEqual(self.mutations.concat_mutations("sample1"), "ACTTACGTACGT")
 
     def test_multiple_non_overlapping_mutations(self):
-        self.mutations.add_sample_mutation("sample2", Mutation("M", 2, "C", 2, "G"))
-        self.mutations.add_sample_mutation("sample2", Mutation("M", 5, "A", 5, "C"))
+        mut_id1 = self.mutations.get_mutation_id(Mutation("M", 2, "C", 2, "G"))
+        mut_id2 = self.mutations.get_mutation_id(Mutation("M", 5, "A", 5, "C"))
+
+        self.mutations.add_sample_mutation("sample2", mut_id1)
+        self.mutations.add_sample_mutation("sample2", mut_id2)
         self.assertEqual(self.mutations.concat_mutations("sample2"), "AGGTCCGTACGT")
 
     def test_mismatched_reference_sequence(self):
-        self.mutations.add_sample_mutation("sample4", Mutation("M", 6, "T", 6, "G"))
+        mut_id = self.mutations.get_mutation_id(Mutation("M", 6, "T", 6, "G"))
+
+        self.mutations.add_sample_mutation("sample4", mut_id)
         # Expecting warning and unchanged sequence due to mismatch
         self.assertEqual(self.mutations.concat_mutations("sample4"), self.transcript_reference)
 
     def test_out_of_bounds(self):
-        self.mutations.add_sample_mutation("sample5", Mutation("M", 13, "T", 13, "GG"))
+        mut_id = self.mutations.get_mutation_id(Mutation("M", 13, "T", 13, "GG"))
+
+        self.mutations.add_sample_mutation("sample5", mut_id)
         # Expecting warning and unchanged sequence due to out-of-bounds
         self.assertEqual(self.mutations.concat_mutations("sample5"), self.transcript_reference)
